3bf058b9dac0cfb5ebb9f9f3daa39451d449f8bc
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-17 16:24:02 +0100

    Defined method

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <?php 
 
 class Movie {
+    public $id;
     public $title;
     public $director;
     public $weft;
     public $published;
     public $genre;    
+    public $views;
+    public $popularity;
     public $resolution;
 
-    public function __construct($title, $director, $weft, $published, $genre, $resolution = '4k') 
+    public function __construct($id, $title, $director, $weft, $published, $genre, $views = null, $popularity = '', $resolution = '4k') 
     {
+        $this->id = $id;
         $this->title = $title;
         $this->director = $director;
         $this->weft = $weft;
         $this->published = $published;
         $this->genre = $genre;
+        $this->views = $views;
+        $this->popularity = $popularity;
         $this->resolution = $resolution;
-
     }
+
+    public function popularity($number) 
+    {
+        if(!is_numeric($number) || $number < 1000) return $this->popularity = 'unpopular';
+        elseif (!is_numeric($number) || $number > 10000) return $this->popularity = 'popular';
+    }
+
 }
+$toTheLake = new Movie(1, 'To the Lake', 'Pavel Kostomarov', 'Il vincitore ai Golden Eagle Kostomarov dirige questa inquietante serie fantastica emozionante, spaventosa e stranamente affascinante', 2020, 'Fantasy', 10100);
+var_dump($toTheLake);
+echo $toTheLake->popularity($toTheLake->views);
 
 ?>
 
