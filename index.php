099bfd68a87e803c7e181f3188d04e0e39029d3a
Antonio-Italiano <antoitalia221@gmail.com>
2023-02-17 17:40:10 +0100

    print on screen movies

--- index.php
+++ index.php
@@ -1,40 +1,14 @@
 <?php 
 
-class Movie {
-    public $id;
-    public $title;
-    public $director;
-    public $weft;
-    public $published;
-    public $genre;    
-    public $views;
-    public $popularity;
-    public $resolution;
+include_once __DIR__ . './data/movies.php';
+include_once __DIR__ . './models/movie.php';
 
-    public function __construct($id, $title, $director, $weft, $published, $genre, $views = null, $popularity = '', $resolution = '4k') 
-    {
-        $this->id = $id;
-        $this->title = $title;
-        $this->director = $director;
-        $this->weft = $weft;
-        $this->published = $published;
-        $this->genre = $genre;
-        $this->views = $views;
-        $this->popularity = $popularity;
-        $this->resolution = $resolution;
-    }
 
-    public function popularity($number) 
-    {
-        if(!is_numeric($number) || $number < 1000) return $this->popularity = 'unpopular';
-        elseif (!is_numeric($number) || $number > 10000) return $this->popularity = 'popular';
-    }
-
+$movies_library = [];
+foreach ($movies as $movie) {
+    $movie_object = new Movie ($movie['id'], $movie['title'], $movie['director'], $movie['weft'], $movie['published'], $movie['genre'], $movie['views'], $movie['resolution']);
+    $movies_library[] = $movie_object;
 }
-$toTheLake = new Movie(1, 'To the Lake', 'Pavel Kostomarov', 'Il vincitore ai Golden Eagle Kostomarov dirige questa inquietante serie fantastica emozionante, spaventosa e stranamente affascinante', 2020, 'Fantasy', 10100);
-var_dump($toTheLake);
-echo $toTheLake->popularity($toTheLake->views);
-
 ?>
 
 <!DOCTYPE html>
@@ -43,9 +17,26 @@ echo $toTheLake->popularity($toTheLake->views);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>PHP Movie</title>
 </head>
 <body>
+    <section id="movie" class="w-25 text-center m-auto mt-5">
+        <?php foreach ($movies_library as $movie) : ?>
+            <div class="col border border-primary my-3">
+                <h1> <?= $movie->id ?> </h1>
+                <p> <?= $movie->published ?> </p>
+                <h4> <?= $movie->genre ?> </h4>
+                <p> <?= $movie->views ?> </p>
+                <h5> <?= $movie->popularity($movie->views) ?> </h5>
+                <h6> <?= $movie->resolution ?> </h6>
+                <h4> <?= $movie->director ?> </h4>
+                <h4> <?= $movie->weft ?> </h4>
+            </div>
+        <?php endforeach; ?>    
+    </section>
     
 </body>
 </html>
